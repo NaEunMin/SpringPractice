6509f2ce7b316691f553c94a70d989b4263ad1f2
naeunmin <babaleo115@gmail.com>
2025-03-28 16:03:45 +0900

    practice

---
+++ src/main/java/com/example/demo/Controller/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller -> 웹 페이지를 렌더링하기 위한 컨트롤러, 뷰 이름을 반환합니다.
+//RestController -> RESTful API를 위한 컨트롤러로, JSON 또는 XML 데이터를 반환합니다.
+// @Controller와 @ResponseBody가 결합된 형태
+@RestController
+public class GreetingController {
+    private final GreetingService greetingService;
+
+    @Autowired //spring이 greetingServie의 구현체인 GreetingServiceLmp를 찾아서 GreetingService 타입으로 주입
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return greetingService.greet();
+    }
+
+}
